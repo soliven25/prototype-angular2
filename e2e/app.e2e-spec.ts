153d6f1443d9cc0a822ba02441b4cf7f9e3ea986
soliven25 <soliven25@gmail.com>
2017-01-16 14:31:52 +0900

    add new project to build angular2 seed project

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { PrototypeAngular2Page } from './app.po';
+
+describe('prototype-angular2 App', function() {
+  let page: PrototypeAngular2Page;
+
+  beforeEach(() => {
+    page = new PrototypeAngular2Page();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
